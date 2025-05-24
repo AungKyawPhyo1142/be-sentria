3f009895c83384c8ca9474ccfa04c1da2dbb52a7
KyiThantSin <kyits485@gmail.com>
2025-05-24 22:41:22 +0700

    feat(user): added profile upload funtions

---
+++ src/middlewares/upload.ts
@@ -0,0 +1,23 @@
+import multer from "multer";
+import { Request } from 'express';
+
+// uploaded files in memory
+const storage = multer.memoryStorage();
+
+const fileFilter = (_req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback ) => {
+    const allowedMimes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif', 'image/webp'];
+
+    if(allowedMimes.includes(file.mimetype)){
+        cb(null, true);
+    }else{
+        cb(new Error('Invalid file type. Only JPEG, PNG, GIF, and WebP images are allowed.'))
+    }
+}
+
+export const upload = multer({
+    storage,
+    fileFilter,
+    limits:{
+        fileSize: 5 * 1024 * 1024, // 5MB limit
+    }
+})
