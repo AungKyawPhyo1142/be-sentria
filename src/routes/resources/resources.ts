39d36d0f046592618d1bff1c1d82aa587b7c31ec
KyiThantSin <kyits485@gmail.com>
2025-06-26 09:40:19 +0700

    feat: update resource endpoints to support multiple image uploads with captions

--- src/routes/resources/resources.ts
+++ src/routes/resources/resources.ts
@@ -7,8 +7,8 @@ const router = Router();
 
 router.get('/', secureRoute(), resourceController.GetResources);
 router.get('/:id', secureRoute(), resourceController.GetResourceById);
-router.post('/create', secureRoute(), upload.single('resourceImage'), resourceController.CreateResource);
-router.patch('/update/:id', secureRoute(), upload.single('resourceImage'), resourceController.UpdateResource);
+router.post('/create', secureRoute(), upload.array('resourceImages', 5), resourceController.CreateResource);
+router.patch('/update/:id', secureRoute(), upload.array('resourceImages', 5), resourceController.UpdateResource);
 router.delete('/delete/:id', secureRoute(), resourceController.DeleteResource);
 
 export default router;
