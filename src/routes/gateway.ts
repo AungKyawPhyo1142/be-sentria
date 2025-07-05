7b750a4804622e5dbe45eeb3f7ebcf0e696d31da
Myra <myra@singtecs.com>
2025-07-05 20:39:10 +0700

    feat: activity feed CRUD

--- src/routes/gateway.ts
+++ src/routes/gateway.ts
@@ -5,6 +5,8 @@ import reportRouter from '@/routes/reports/reports';
 import resourceRouter from '@/routes/resources/resources';
 import userRouter from '@/routes/user/user';
 import followersRouter from '@/routes/followers/followers';
+import activityRouter from '@/routes/activity/activity';
+
 import { Router } from 'express';
 
 const gateway = Router();
@@ -16,5 +18,6 @@ gateway.use('/users', userRouter);
 gateway.use('/resource', resourceRouter);
 gateway.use('/goTest', goTestRouter);
 gateway.use('/followers', followersRouter);
+gateway.use('/activity', activityRouter);
 
 export default gateway;
